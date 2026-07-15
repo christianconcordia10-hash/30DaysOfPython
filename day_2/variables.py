7d1ee48924aded6abbffa643b56bc9b36c2910af
christianconcordia10-hash <christianconcordia10@gmail.com>
2026-07-15 14:40:59 -0400

    Create variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,14 @@
+  # Day 2: 30 Days of python programming
+   first_name = "Christian"
+   last_name = "Concordia"
+   full_name = first_name + " " + last_name
+   country = "USA"
+   city = "Philadelphia"
+   age = 20
+   year = 2026
+   is_married = False
+   is_true = True
+   is_light_on = True
+
+   # multiple variables on one line
+   a, b, c = 1, 2, 3
