04cebd7f42b52951ccf32f31768db689ecc64e53
christianconcordia10-hash <christianconcordia10@gmail.com>
2026-07-17 14:47:56 -0400

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -12,3 +12,55 @@
 
    # multiple variables on one line
    a, b, c = 1, 2, 3
+  # check data type of all variables
+print(type(first_name))
+print(type(last_name))
+print(type(full name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+#lenght of first name
+print(len(first_name)
+#compare length of first name to last name
+print(len(first_name)
+print(len(last_name)
+#declare 5 and num_1 and 4 as num_2
+5 = num_one
+4 = num_two
+# add num_one and num_two and assingn a variable to the total
+x = num_one + num_two
+# subtract num two from num one and assign its value to a diff varaible
+k = num_one - num_two
+# Multiply num_two and num_one and assign the value to a variable product
+varialbe_product = num_two * num_one
+# Divide num_one by num_two and assign the value to a variable division
+ variable_divison = num_one / num_two
+# Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+variable_remainder = num_two % num_one
+# Calculate num_one to the power of num_two and assign the value to a variable exp
+variable_exp = num_one ** num_two
+# Find floor division of num_one by num_two and assign the value to a variable floor_division
+floor_divison = num_one // num_two
+# The radius of a circle is 30 meters.
+#Calculate the area of a circle and assign the value to a variable name of area_of_circle\
+radius = 30
+i = radius ** 2
+area_of_circle = i * math.pi
+
+#Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+q = math.pi * 30
+circum_of_circle = q * 2
+#Take radius as user input and calculate the area.
+x = input("radius: "
+r = x **2
+area= r * math.pi
+
+# get last fisrst name country age
+input("Whats your name? ")
+input("Whats your last name? ")
+inout("Whats your age? ")
+input("What country are you from? ")
